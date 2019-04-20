86515639cfe73680823fd32ecf329f5d80c64ea7
Yiming <zym1995osl@163.com>
2019-04-20 20:46:02 +1000

    Complete the coding of the second map.

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ var myIcon2 = L.icon({
     popupAnchor: [-3, -76],
 });
 
+
 // save some places for testing
 
 var array = [];
@@ -48,11 +49,12 @@ var coordinates = [
     [-37.77859436217878, 144.94537353515625]
 ];
 
+
 coordinates.forEach(function(coordinate){
     //var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.').addTo(mymap1);
-    var markerMap = L.marker((coordinate), {icon: myIcon2}).bindPopup('Here is a Twitter.');
+    var markerMap = L.marker((coordinate), {icon: myIcon2}).bindPopup('Normal Twitter.');
     array.push(markerMap);
-    console.log(coordinate);
+    // console.log(coordinate);
 });
 
 var southBank      = L.marker([-37.814700770115996,  144.91597290039062],{icon: myIcon}).bindPopup('Twitter from SouthBank.'),
@@ -69,13 +71,13 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-    "twitters": places,
-    "Other twitters": places2
+    "Normal twitters": places,
+    "Wrath twitters": places2
 };
 
 // map has restricted area and zoom range form 11-13
-var southWest = L.latLng(-37.99869780196609, 144.56522216796875),
-    northEast = L.latLng(-37.61804716978352,  145.2781463623047);
+var southWest = L.latLng(-38.00869780196609, 144.560522216796875),
+    northEast = L.latLng(-37.61804716978352,  145.2981463623047);
 mymap1.setMaxBounds(new L.LatLngBounds(southWest, northEast));
 
 L.control.layers(baseMaps, overlayMaps).addTo(mymap1);
@@ -114,7 +116,7 @@ info.onAdd = function (map) {
 
 // method that we will use to update the control based on feature properties passed
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Melbourne GEO Info</h4>' +  (props ?
+    this._div.innerHTML = '<h5>Wrath story map</h5>' +  (props ?
         '<b>' + props.name + '</b><br />' + 'ID ' + props.cartodb_id
         : 'Hover over a polygon');
 };
@@ -148,7 +150,7 @@ function highlightFeature(e) {
 
     layer.setStyle({
         weight: 5,
-        color: '#666',
+        color: '#F5F5F5',
         dashArray: '',
         fillOpacity: 0.7
     });
@@ -211,16 +213,30 @@ var mymap2 = L.map('mapid2',{
     maxZoom: 12,
     minZoom: 11,
     layers: [base1, base2]
-}).setView([-37.81358124698001,144.96665954589844], 12);
+}).setView([-37.81358124698001,144.96665954589844], 11);
 
-var southWest = L.latLng(-37.89869780196609, 144.66522216796875),
-    northEast = L.latLng(-37.71804716978352,  145.1781463623047);
+var southWest = L.latLng(-38.00869780196609, 144.560522216796875),
+    northEast = L.latLng(-37.61804716978352,  145.2981463623047);
 
 mymap2.setMaxBounds(new L.LatLngBounds(southWest, northEast));
 
 // add icon to marker
 var myIcon = L.icon({
-    iconUrl: './assets/twitter.png',
+    iconUrl: './assets/twitterShadow.png',
+    iconSize: [20, 20],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+});
+
+var angryIcon = L.icon({
+    iconUrl: './assets/angry.png',
+    iconSize: [20, 20],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+});
+
+var coolIcon = L.icon({
+    iconUrl: './assets/cool.png',
     iconSize: [20, 20],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76],
@@ -233,8 +249,6 @@ var baseMaps = {
     "basemap 2": base2
 };
 
-L.control.layers(baseMaps).addTo(mymap2);
-
 var array = [];
 var coordinates = [
     [-37.854700770115996,  144.96597290039062],
@@ -242,14 +256,153 @@ var coordinates = [
     [-37.77859436217878, 144.94537353515625]
 ];
 
+var angry = []
+var coordinatesAngry = [
+    [-37.814700770115996,  144.92597290039062],
+    [-37.824700770115996,  144.93597290039062],
+    [-37.79859436217878, 144.94537353515625]
+];
+
+var cool = []
+var coordinatesCool = [
+    [-37.824700770115996,  144.91597290039062],
+    [-37.844700770115996,  144.94597290039062],
+    [-37.78859436217878, 144.93537353515625]
+];
+
 coordinates.forEach(function(coordinate){
-    var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.').addTo(mymap2);
-    //var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.');
-    //array.push(markerMap);
+    //var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.').addTo(mymap2);
+    var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.');
+    array.push(markerMap);
     //console.log(coordinate);
 });
 
-    L.geoJson(jsonData2).addTo(mymap2);
+coordinatesAngry.forEach(function(coordinate){
+    //var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.').addTo(mymap1);
+    var markerMap = L.marker((coordinate), {icon: angryIcon}).bindPopup('Angry Twitter.');
+    angry.push(markerMap);
+});
+
+coordinatesCool.forEach(function(coordinate){
+    //var markerMap = L.marker((coordinate), {icon: myIcon}).bindPopup('Here is a Twitter.').addTo(mymap1);
+    var markerMap = L.marker((coordinate), {icon: coolIcon}).bindPopup('Cool Twitter.');
+    cool.push(markerMap);
+});
+
+var places = L.layerGroup(array);
+var places3 = L.layerGroup(angry);
+var places4 = L.layerGroup(cool);
+
+var overlayMaps = {
+    "Normal twitters": places,
+    "Angry twitters": places3,
+    "Cool twitters": places4
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(mymap2);
+
+function getColor(d) {
+    return d > 100 ? '#3300FF' :
+           d > 50  ? '#3366FF' :
+           d > 20  ? '#3399FF' :
+           d > 10  ? '#33CCFF' :
+           d > 5   ? '#33FFFF' :
+           d > 2   ? '#6633FF' :
+           d > 0   ? '#6666FF' :
+                      '#6666FF';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.cartodb_id),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+// custom info control
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h5>Lust story map</h5>' +  (props ?
+        '<b>' + props.name + '</b><br />' + 'ID ' + props.cartodb_id
+        : 'Hover over a polygon');
+};
+
+info.addTo(mymap2);
+
+// legend of the heat map
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 2, 5, 10, 20, 50, 100],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(mymap2);
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#F5F5F5',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson2.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    mymap1.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+// add polygon layer to the map
+geojson2 = L.geoJson(jsonData2.responseJSON, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(mymap2);
+
+
 });
 
 
