a50b33858c8f78712ad02e8e9f1c287244e25b9a
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-04-30 20:26:37 +1000

    bash script for curling tweets from richard's db

---
+++ harvesters/curl_tweet_by_loc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -XGET
+"http://45.113.232.90/couchdbro/twitter/_design/twitter/_list/geojson/geoindex?reduce=false" \
+-G \
+--data-urlencode 'start_key=["r1r0",null,null,null]' \
+--data-urlencode 'end_key=["r1r1",{},{},{}]' \
+--user "readonly:ween7ighai9gahR6"
+-o tmp/tweet_by_loc.json
