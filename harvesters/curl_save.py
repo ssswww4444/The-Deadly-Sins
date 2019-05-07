dda5b3b65dddcca7cfe3bba2e2e16110518edefc
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-05-07 13:16:58 +1000

    Revert "Revert "fix file""
    This reverts commit 75a3a982d1c0e3abcbc4e2c6b5746135adb48e42.

--- harvesters/curl_save.py
+++ harvesters/curl_save.py
@@ -3,58 +3,67 @@ import argparse
 from twitter_db import TweetStore
 import os
 import subprocess
+import sys
 
 # json files
 JSON_PATH = "json_files/"
-DB_AUTH_FILENAME = "db_auth.json"
-DATA_DIR = "/mnt/twitter/"
-LOC_FILE = "tweet_by_loc.json"
-DATE_FILE = "tweet_by_date.json"
-CURL_LOC_BASH = "curl_tweet_by_date.sh"
-CURL_DATE_BASH = "curl_tweet_by_date.sh"
+DATA_PATH = "/mnt/twitter/"
+BASH_PATH = "bash_files/"
+FILE_DICT = {
+    "db_auth": "db_auth.json"
+    "loc": "tweet_by_loc.json"
+    "date": "tweet_by_date.json"
+    "loc_bash": "curl_tweet_by_date.sh"
+    "date_bash": "curl_tweet_by_date.sh"
+}
 
 def get_args():
     """ Obtaining args from terminal """
     parser = argparse.ArgumentParser(description="Curl tweets from DB and save to another DB")
     # filenames
-    parser.add_argument("-db", "--db_name", type = str, required = True, help = "Name of Database for storing")
+    parser.add_argument("-db", "--db_name", type = str, required = True, 
+                        help = "Name of Database for storing")
+    parser.add_argument("-y", "--year", type = str, required = True, 
+                        help = "Year of tweets to get")
+    parser.add_argument("-m", "--month", type = str, required = True, 
+                        help = "Month of tweets to get")
     args = parser.parse_args()
     
     return args
 
 def curl_by_loc():
     """ Curl tweets by location """
-    if os.path.exists(DATA_DIR + LOC_FILE):
+    if os.path.exists(DATA_PATH + FILE_DICT["loc"]):
         return
-    subprocess.call(["bash", CURL_LOC_BASH])
+    subprocess.call(["bash", BASH_PATH + FILE_DICT["loc_bash"]])
 
-def curl_by_date():
+def curl_by_date(year, month):
     """ Curl tweets by date """
-    if os.path.exists(DATA_DIR + DATE_FILE):
+    if os.path.exists(DATA_PATH + FILE_DICT["date"]):
         return
-    subprocess.call(["bash", CURL_DATE_BASH])
+    subprocess.call(["bash", BASH_PATH + FILE_DICT["date_bash"], year, month])
 
 def read_jsons():
     """ Read required json data files """
-    with open(JSON_PATH + DB_AUTH_FILENAME, "r") as f:
+    with open(JSON_PATH + FILE_DICT["db_auth"], "r") as f:
         db_auth = json.load(f)
 
     return db_auth
 
-def read_tweets(filename, storage):
+def read_tweets(filename, storage, year, month):
     """ read each tweet from file and save to db """
+    filename = DATA_PATH + filename[:-5] + f"_{args.year}_{args.month}" + ".json"
     with open(filename, "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
 
             # first / last line
-            if i == 0 or (i % (size) != rank):  # assign line according to the rank
-                continue
-            elif line[0] != "{":
+            if i == 0 or line[0] != "{":
                 continue
 
             # load cotent and save
             tweet = json.loads(fix_line(line))
-            storage.save_tweet(tweet)
+            storage.save_tweet(tweet["doc"])
+                
         f.close()
 
 def main():
@@ -72,12 +81,12 @@ def main():
     storage = TweetStore(args.db_name, url)
 
     # curl if no json files found
-    curl_by_loc()
-    curl_by_date()
+    # curl_by_loc()
+    curl_by_date(args.year, args.month)
 
     # read each json file and upload to db
-    read_tweets(LOC_FILE, storage)
-    read_tweets(DATE_FILE, storage)
+    # read_tweets(FILE_DICT["loc"], storage, args)
+    read_tweets(FILE_DICT["date"], storage, args)
 
 if __name__ == "__main__":
     main()
