c98cf8bcd5c672b53b4a56aab882d2a03546449d
Wenqi Sun <wenqisun0415@gmail.com>
2019-05-07 17:42:31 +1000

    Add codes for spark connect with couchdb

--- spark/spark.py
+++ spark/spark.py
@@ -3,87 +3,9 @@ import json
 from Zones import Zones
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-
-conf = SparkConf().setAppName("test").setMaster("local[*]")
-sc = SparkContext(conf=conf)
-twitter_file = sc.textFile("../../twitter.json")
-
-import pickle
-import nltk
-import re
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-from nltk.tokenize.casual import TweetTokenizer
-from nltk.stem import WordNetLemmatizer
-import emoji
-
-nltk.download("wordnet", "/Users/wenqisun/nltk_data/")
-nltk.data.path.append("/Users/wenqisun/Downloads/nltk_data/")
-
-stop_words = set(stopwords.words('english'))
-lemmatizer = WordNetLemmatizer()
-
-# load in classifier and vectorizer
-logistic_clf = pickle.load(open("../../Logistic_{C0.1}_{l1}.pk1", "rb"))  
-vec = pickle.load(open("../../DictVectorizer.pk1", "rb"))
-
-frequent_symbols = [":)", ":(", ":D", ":-)", ":p", ";)", ":/", "XD"]
-
-# Get array of dictionary according to the list of tweets 
-def get_dict(tweets):
-    dict_arr = []
-    for tweet in tweets:
-        tweet_dict = {}
-        for word in tweet:
-            if word in tweet_dict:
-                tweet_dict[word] += 1
-            else:
-                tweet_dict[word] = 1
-        dict_arr.append(tweet_dict)
-    return dict_arr
-
-def handle_hashtag(hashtag):
-    i = 0
-    while(hashtag[i] == "#"):
-        i += 1
-        if i == len(hashtag):
-            return -1
-    
-    # multiple hashtags in the word
-    if "#" in hashtag[i:]:
-        return -1
-    
-    return hashtag[i:]
-
-def preprocess(tweets):
-    new_tweets = []
-    for tweet in tweets:
-        new_tweet = []
-        for word in tokenizer.tokenize(tweet):
-
-            # regular words
-            if not (word in stop_words or re.search("[^a-zA-Z]",word)):  # if not stopwords and non-alphabetic
-                new_tweet.append(lemmatizer.lemmatize(word))
-            
-            # hashtags
-            elif word[0] == "#":  
-                hashtag = handle_hashtag(word)
-                if hashtag != -1:
-                    new_tweet.append("#" + lemmatizer.lemmatize(hashtag))
-            
-            # symbols / emojis
-            elif word in frequent_symbols or word in emoji.UNICODE_EMOJI:
-                new_tweet.append(word)
-                
-        new_tweets.append(new_tweet)
-    
-    dict_arr = get_dict(new_tweets)
-    return (dict_arr)
-
-
-# tokenizer will downcase everything except for emoticons
-tokenizer = TweetTokenizer(preserve_case=False)
-
+from textblob import TextBlob
+import couchdb
+from pyspark.sql import SparkSession
 
 def isContent(line: str):
     return line.startswith("{\"i")
@@ -118,27 +40,88 @@ def in_polygon(tweet):
     return 'unknown'
 
 def negative_tweet(tweet):
-    tweet_dicts = preprocess([tweet['text']])
-    X = vec.transform(tweet_dicts)
-    result = logistic_clf.predict(X)[0]  # 1 for positive, 0 for negative
-    if(result == 0):
+    result = TextBlob(tweet['text'])
+    if(result.sentiment.polarity < 0):
         return True
     else:
         return False
 
+def find_count(polygon_code, dict):
+    if(polygon_code in dict):
+        return dict[polygon_code]
+    else:
+        return 0
+
+# This part is for reading from a local file
+# conf = SparkConf().setAppName("test").setMaster("local[*]")
+# sc = SparkContext(conf=conf)
+# twitter_file = sc.textFile("../../../twitter.json")
+
+
+# This part is for connecting to couchdb
+spark = SparkSession.builder.getOrCreate()
+
+twitter_file = spark.read.format("org.apache.bahir.cloudant")\
+    .option("cloudant.protocol", "http")\
+    .option("cloudant.host", "45.113.233.243:5984")\
+        .option("cloudant.username", "admin")\
+            .option("cloudant.password", "123qweasd")\
+                .load("twitter_with_loc")
+
 zones = Zones().zones
-twitters = twitter_file.filter(isContent)
-twitter_contents = twitters.map(extractContent)
-twitter_with_coor = twitter_contents.filter(hasCoordinates)
-twitter_by_polygon = twitter_with_coor.map(in_polygon)
+twitters = twitter_file.rdd
 
-twitter_negative = twitter_with_coor.filter(negative_tweet)
+twitter_by_polygon = twitters.map(in_polygon)
 
-num_tweet_by_polygon = twitter_by_polygon.countByValue()
+num_twitter_by_polygon = twitter_by_polygon.countByValue()
+
+# Writing result back to database
+couchserver = couchdb.Server("http://admin:123qweasd@45.113.233.232:5984/")
+db = couchserver["job_json"]
+
+tweet_dict = {}
+#nega_tweet_dict = {}
+for polygon, count in num_twitter_by_polygon.items():
+    tweet_dict[polygon] = count
+
+for tweet in db:
+    tweet_con = db[tweet]
+    for feature in tweet_con["features"]:
+        pro = feature["properties"]
+        pro["num_tweets"] = find_count(pro["sa2_code16"], tweet_dict)
+        #pro["num_negative_tweets"] = find_count(pro["sa2_code16"], nega_tweet_dict)
+    db.save(tweet_con)
 
 
-print("NUM of twitters: {}".format(twitter_contents.count()))
-print("NUM of twitters with coor: {}".format(twitter_with_coor.count()))
-print("NUM of negative twitters: {}".format(twitter_negative.count()))
-# for polygon, count in num_tweet_by_polygon.items():
-#     print("{}, {}".format(polygon, count))
+
+# twitters = twitter_file.filter(isContent)
+# twitter_contents = twitters.map(extractContent)
+# twitter_with_coor = twitter_contents.filter(hasCoordinates)
+# twitter_by_polygon = twitter_with_coor.map(in_polygon).countByValue()
+
+# for polygon, count in twitter_by_polygon.items():
+#     print("{}: {}".format(polygon, count))
+
+
+
+#print("The type is {}".format(type(twitter_by_polygon)))
+
+# twitter_negative = twitter_with_coor.filter(negative_tweet)
+# twitter_negative_by_polygon = twitter_negative.map(in_polygon)
+
+# num_tweet_by_polygon = twitter_by_polygon.countByValue()
+# num_negative_tweet_by_polygon = twitter_negative_by_polygon.countByValue()
+
+
+
+# # num_tweet_by_polygon.saveAsTextFile("out/twitter_by_polygon.json")
+# # num_negative_tweet_by_polygon.saveAsTextFile("out/twitter_negative_by_polygon.json")
+# print("Num of negative tweets in each polygon")
+
+# for polygon, count in num_negative_tweet_by_polygon.items():
+#     nega_tweet_dict[polygon] = count
+
+
+
+
+
