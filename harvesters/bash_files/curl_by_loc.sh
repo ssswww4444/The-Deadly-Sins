aa12a7ec9d2dae7032feda18981207d54564ce46
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-05-07 16:31:47 +1000

    add sudo

--- harvesters/bash_files/curl_by_loc.sh
+++ harvesters/bash_files/curl_by_loc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Usage: sh curl_by_loc.sh
 if [ ! -d "/mnt/twitter" ]; then
-  mkdir "/mnt/twitter"
+  sudo mkdir "/mnt/twitter"
 fi
-curl -XGET "http://45.113.232.90/couchdbro/twitter/_design/twitter/_list/geojson/geoindex?reduce=false" \
+sudo curl -XGET "http://45.113.232.90/couchdbro/twitter/_design/twitter/_list/geojson/geoindex?reduce=false" \
 -G \
 --data-urlencode 'start_key=["r1r0",null,null,null]' \
 --data-urlencode 'end_key=["r1r1",{},{},{}]' \
