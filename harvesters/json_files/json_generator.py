57b0b54daded8e85fb80a6591ff93b9e99f06ddc
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-05-07 12:54:59 +1000

    mv file

--- harvesters/json_files/json_generator.py
+++ harvesters/json_files/json_generator.py
@@ -1,14 +1,12 @@
 import json
 
-JSON_PATH = "json_files/"
-
 # bounding box file
 bounding = {}
 bounding["Victoria"] = "140.961681984, -39.159189527500004, 149.976679008, -33.9806475865"
 bounding["Australia"] = "96.81676569599999, -43.740509603, 159.109219008, -9.142175977"
 
 
-with open(JSON_PATH + "bounding_box.json", "w") as f:
+with open("bounding_box.json", "w") as f:
     json.dump(bounding, f)
 
 # Twitter API authentication credentials
@@ -25,7 +23,7 @@ auth["ACCESS_TOKEN_SECRET"] = "CMckhw2NaULIqKnp5DcIqcwJaE1nAyfT2SMqzbnxllGoY"
 # auth["ACCESS_TOKEN"] = "1107074333698981889-bzffZA2f5djoFeV30veP1oHBLNXeab"
 # auth["ACCESS_TOKEN_SECRET"] = "CMckhw2NaULIqKnp5DcIqcwJaE1nAyfT2SMqzbnxllGoY"
 
-with open(JSON_PATH + "twitterAPI_auth.json", "w") as f:
+with open("twitterAPI_auth.json", "w") as f:
     json.dump(auth, f)
 
 # DB authentication
@@ -34,5 +32,5 @@ db_auth["ip"] = "45.113.233.232"
 db_auth["port"] = "5984"
 db_auth["user"] = "admin" 
 db_auth["pwd"] = "123qweasd"
-with open(JSON_PATH + "db_auth.json", "w") as f:
+with open("db_auth.json", "w") as f:
     json.dump(db_auth, f)
