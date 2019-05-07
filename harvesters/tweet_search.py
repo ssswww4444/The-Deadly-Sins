9776b34be2ac774a0aa7955f748f1dc09aecd1e6
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-05-07 17:05:10 +1000

    using search api

---
+++ harvesters/tweet_search.py
@@ -0,0 +1,73 @@
+from TwitterAPI.TwitterAPI import TwitterAPI
+import json
+import argparse
+from db import TweetStore
+import sys
+import threading
+
+# json files
+JSON_PATH = "json_files/"
+FILE_DICT = {
+    "db_auth": "db_auth.json",
+    "twitter_api": "twitterAPI_auth.json"
+}
+uid_ls = []
+
+def get_args():
+    """ Obtaining args from terminal """
+    parser = argparse.ArgumentParser(description="Processing tweets")
+    # filenames
+    parser.add_argument("-db", "--db_name", type = str, required = True, 
+                        help = "Name of Database for storing")
+    parser.add_argument("-q", "--query", type = str, required = True, 
+                        help = "Query")
+
+    args = parser.parse_args()
+    
+    return args
+
+def read_jsons():
+    """ Read required json files """
+    with open(JSON_PATH + FILE_DICT["db_auth"], "r") as f:
+        db_auth = json.load(f)
+
+    # Twitter authentication credentials
+    with open(JSON_PATH + FILE_DICT["twitter_api"], "r") as f:
+        api_auth = json.load(f)
+
+    return db_auth, api_auth
+
+def twitter_search(api, storage, query):
+    """ Real-time twitter streaming """
+    # requesting tweets (in bounding box of specified region)
+    for item in api.request("search/tweets", {"q": query, "count": 100}):
+        if "text" in item:
+            print('STREAM: %s -- %s\n' % (item['user']['screen_name'], item['text']))
+            # save tweet to database
+            storage.save_tweet(item)
+        elif 'message' in item:
+            print('ERROR %s: %s\n' % (item['code'], item['message']))
+
+def main():
+
+    # get arguments
+    args = get_args()
+
+    # read required json files
+    db_auth, api_auth = read_jsons()
+
+    # db url
+    url = "http://" + db_auth["user"] + ":" + db_auth["pwd"] \
+                    + "@" + db_auth["ip"] + ":" + db_auth["port"] + "/"
+
+    # initialise db and twitter api
+    storage = TweetStore(args.db_name, url)
+    api = TwitterAPI(api_auth["API_KEY"],
+                     api_auth["API_SECRET"], 
+                     api_auth["ACCESS_TOKEN"], 
+                     api_auth["ACCESS_TOKEN_SECRET"])
+
+    twitter_search(api, storage, args.query)
+
+if __name__ == "__main__":
+    main()
