ffd30487e9f4434e67ef90421b34c6e4e09e5a8d
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-05-07 12:54:13 +1000

    get user timeline

--- harvesters/tweet_mining.py
+++ harvesters/tweet_mining.py
@@ -1,43 +1,77 @@
 from TwitterAPI.TwitterAPI import TwitterAPI
 import json
 import argparse
-from twitter_db import TweetStore
+from db import TweetStore
+import sys
+import threading
 
 # json files
 JSON_PATH = "json_files/"
-DB_AUTH_FILENAME = "db_auth.json"
-TWITTER_API_FILENAME = "twitterAPI_auth.json"
-BOUNDING_FILENAME = "bounding_box.json"
+FILE_DICT = {
+    "db_auth": "db_auth.json"
+    "twitter_api": "twitterAPI_auth.json"
+    "bounding": "bounding_box.json"
+}
+uid_ls = []
 
 def get_args():
     """ Obtaining args from terminal """
     parser = argparse.ArgumentParser(description="Processing tweets")
     # filenames
-    parser.add_argument("-db", "--db_name", type = str, required = True, help = "Name of Database for storing")
+    parser.add_argument("-db", "--db_name", type = str, required = True, 
+                        help = "Name of Database for storing")
+    parser.add_argument("-re", "--region", type = str, required = True, 
+                        help = "Region for streaming (Australia / Victoria)")
+
     args = parser.parse_args()
     
     return args
 
 def read_jsons():
+    """ Read required json files """
     # bounding boxes for twitter stream API
-    with open(JSON_PATH + BOUNDING_FILENAME, "r") as f:
+    with open(JSON_PATH + FILE_DICT["bounding"], "r") as f:
         bounding = json.load(f)
 
-    with open(JSON_PATH + DB_AUTH_FILENAME, "r") as f:
+    with open(JSON_PATH + FILE_DICT["db_auth"], "r") as f:
         db_auth = json.load(f)
 
     # Twitter authentication credentials
-    with open(JSON_PATH + TWITTER_API_FILENAME, "r") as f:
+    with open(JSON_PATH + FILE_DICT["twitter_api"], "r") as f:
         api_auth = json.load(f)
 
     return bounding, db_auth, api_auth
 
-def twitter_streaming(api, storage, bounding):
-    # requesting tweets (in bounding box of Victoria)
-    for item in api.request("statuses/filter", {"locations": bounding["Victoria"]}):
+def twitter_user_timeline(api, storage):
+    """ Getting tweets from user timeline """
+    while True:
+        # wait until has task
+        if(len(uid_ls) == 0):
+            continue
+
+        # requesting timeline
+        uid = uid_ls.pop(0)
+
+        for item in api.request("statuses/user_timeline", {"user_id": uid, "count": 200):
+            if "text" in item:
+                print('USER: %s -- %s\n' % (item['user']['screen_name'], item['text']))
+                # save tweet to database
+                storage.save_tweet(item)
+            elif 'message' in item:
+                print('ERROR %s: %s\n' % (item['code'], item['message']))
+
+
+def twitter_streaming(api, storage, bounding, region):
+    """ Real-time twitter streaming """
+    # requesting tweets (in bounding box of specified region)
+    for item in api.request("statuses/filter", {"locations": bounding[region]}):
         if "text" in item:
-            print('%s -- %s\n' % (item['user']['screen_name'], item['text']))
-            storage.save_tweet(item)    # save tweet to database
+            print('STREAM: %s -- %s\n' % (item['user']['screen_name'], item['text']))
+            # save tweet to database
+            storage.save_tweet(item)
+            # only get timeline for user tweeted with coordinates
+            if item["coordinates"]:
+                uid_ls.append(item["user"]["id"])
         elif 'message' in item:
             print('ERROR %s: %s\n' % (item['code'], item['message']))
 
@@ -60,8 +94,12 @@ def main():
                      api_auth["ACCESS_TOKEN"], 
                      api_auth["ACCESS_TOKEN_SECRET"])
 
-    # start streaming
-    twitter_streaming(api, storage, bounding)
+    t1 = threading.Thread(target=twitter_streaming, args=(api, storage, bounding, args.region))
+    t2 = threading.Thread(target=twitter_user_timeline, args=(api, storage))
+
+    # start streaming and getting timelines
+    t1.start()
+    t2.start()
 
 if __name__ == "__main__":
     main()
