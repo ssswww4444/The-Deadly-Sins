5e2ac501bf2517e5f4ae26ee9e7d5e0aa653f401
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2019-05-07 12:50:50 +1000

    add folder

---
+++ harvesters/bash_files/curl_by_date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Usage: sh curl_by_date.sh year month
+if [ ! -d "/mnt/twitter" ]; then
+  mkdir "/mnt/twitter"
+fi
+curl "http://45.113.232.90/couchdbro/twitter/_design/twitter/_view/summary" \
+-G \
+--data-urlencode 'start_key=["melbourne", $1,$2,1]' \
+--data-urlencode 'end_key=["melbourne",$1,$2,31]' \
+--data-urlencode 'reduce=false' \
+--data-urlencode 'include_docs=true' \
+--user "readonly:ween7ighai9gahR6" \
+-o /mnt/twitter/tweet_by_date_$1_$2.json
--- harvesters/bash_files/curl_by_loc.sh
+++ harvesters/bash_files/curl_by_loc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ ! -d "/tmp/twitter" ]; then
-  mkdir "/tmp/twitter"
+# Usage: sh curl_by_loc.sh
+if [ ! -d "/mnt/twitter" ]; then
+  mkdir "/mnt/twitter"
 fi
 curl -XGET "http://45.113.232.90/couchdbro/twitter/_design/twitter/_list/geojson/geoindex?reduce=false" \
 -G \
 --data-urlencode 'start_key=["r1r0",null,null,null]' \
 --data-urlencode 'end_key=["r1r1",{},{},{}]' \
 --user "readonly:ween7ighai9gahR6" \
--o /tmp/twitter/tweet_by_loc.json
+-o /mnt/twitter/tweet_by_loc.json
