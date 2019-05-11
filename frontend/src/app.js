ec71d0c11f4d53e67ec66cb3cfd3d63213a2d0f1
Yiming <zym1995osl@163.com>
2019-05-11 18:35:59 +1000

    Add a pie chart,word cloud, scatter chart and predicition.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,6 +7,7 @@ var jsonData = $.ajax({
         // AURIN Job dataset API: http://45.113.233.243:5984/job_json/90a80a4fc502d169ebce4ee07a001fb8
         // testing melbourne geo json API: http://admin:123qweasd@45.113.233.243:5984/geo_json/2adb959d243ca8869f8d9576bb0028c2
         url:"http://admin:123qweasd@45.113.233.243:5984/job_json/90a80a4fc502d169ebce4ee07a001fb8",
+        //url: "https://api.myjson.com/bins/xefh8",
         type: "GET",
         dataType: "json",
         success: console.log("GeoJson successfully loaded from couchDB."),
@@ -194,7 +195,8 @@ geojson = L.geoJson(jsonData.responseJSON, {
 // map2
 // https://api.myjson.com/bins/xefh8
 var jsonData2 = $.ajax({
-    url:"http://admin:123qweasd@45.113.233.243:5984/geo_json/2adb959d243ca8869f8d9576bb0028c2",
+   //url:"http://admin:123qweasd@45.113.233.243:5984/geo_json/2adb959d243ca8869f8d9576bb0028c2",
+    url: "https://api.myjson.com/bins/xefh8",
     type: "GET",
     dataType: "json",
     success: console.log("GeoJson successfully loaded from couchDB."),
@@ -411,36 +413,42 @@ geojson2 = L.geoJson(jsonData2.responseJSON, {
 
 // chart part for wrath story
 var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'horizontalBar',
+var dataset1x, dataset1y,dataset2x,dataset2y;
 
-    // The data for our dataset
-    data: {
-        labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
-        datasets: [{
-            label: 'Number',
-            backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
-            borderColor:["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],
-            data: [82, 50, 60, 30, 40, 30, 45]
+var feature1 = [10,20,30,40,50,60,70,55,78,99,100]
+var feature2 = [20,30,40,50,10,20,80,66,66,11,12]
+var data1 = feature1.map( (v,i) => ({ x: v, y: feature2[i] }) )
+var scatterChartData1 = {
+    datasets: [{
+        label: 'My First dataset',
+        borderColor: '#3e95cd',
+        backgroundColor: '#3e95cd',
+        data: data1
+        
+    }, {
+        label: 'My Second dataset',
+        borderColor:  "#8e5ea2",
+        backgroundColor: "#8e5ea2",
+        data: [{
+            x: 12,
+            y: 15
+        }, {
+            x: 13,
+            y: 12
         }]
-    },
+    }]
+};
 
-    // Configuration options go here
-    options: {
-        legend: {
-            display: false
-        },
-
-        scales: {
-            xAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+ var scatterChart = new Chart(ctx, {
+        type: 'scatter',
+        data: scatterChartData1,
+        options: {
+            title: {
+                display: true,
+                text: 'Wrath Scatter Chart'
+            },
         }
-    }
-});
+    });
 
 
 // pie chart
@@ -458,50 +466,129 @@ var myPieChart = new Chart(ctx3, {
     options: {
         title: {
           display: true,
-          text: 'Wrath story'
+          text: 'Melbourne Pie Chart'
         }
     }
 });
 
+$("#table-select1 a").click(function(e){
+    e.preventDefault(); // cancel the link behaviour
+    var selText1 = $(this).text();
+    console.log(selText1)
+
+    if (selText1 === "Melbourne"){
+
+        var pieChartContent1 = document.getElementById('pieChartContent1');
+        pieChartContent1.innerHTML = '&nbsp;';
+        $('#pieChartContent1').append(' <canvas id="pie-chart" width="400" height="200"><canvas>');
+        var ctx3 = document.getElementById('pie-chart').getContext('2d');
+
+        var myPieChart  = new Chart(ctx3, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [82, 50, 60, 30, 40, 30, 45]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Wrath: Melbourne Pie Chart'
+                }
+            }
+        });
+    }
+
+    if (selText1 === "North Melbourne"){
+
+        var pieChartContent1 = document.getElementById('pieChartContent1');
+        pieChartContent1.innerHTML = '&nbsp;';
+        $('#pieChartContent1').append(' <canvas id="pie-chart" width="400" height="200"><canvas>');
+        var ctx3 = document.getElementById('pie-chart').getContext('2d');
+        
+        var myPieChartt = new Chart(ctx3, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [12, 20, 10, 90, 80, 70, 25]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Wrath: North Melbourne Pie Chart'
+                }
+            }
+        });
+    }
+
+    if (selText1 === "Richmond"){
+
+        var pieChartContent1 = document.getElementById('pieChartContent1');
+        pieChartContent1.innerHTML = '&nbsp;';
+        $('#pieChartContent1').append(' <canvas id="pie-chart" width="400" height="200"><canvas>');
+        var ctx3= document.getElementById('pie-chart').getContext('2d');
+        
+        var myPieChart = new Chart(ctx3, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [36, 29, 80, 20, 40, 30, 95]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Wrath: Richmond Pie Chart'
+                }
+            }
+        });
+    }
+    if (selText1 === "South Melbourne"){
+
+        var pieChartContent1 = document.getElementById('pieChartContent1');
+        pieChartContent1.innerHTML = '&nbsp;';
+        $('#pieChartContent1').append(' <canvas id="pie-chart" width="400" height="200"><canvas>');
+        var ctx3 = document.getElementById('pie-chart').getContext('2d');
+        
+        var myPieChart = new Chart(ctx3, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [42, 10, 40, 20, 10, 70, 25]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Wrath: South Melbourne Pie Chart'
+                }
+            }
+        });
+    }
+    $("#table-button1").text(selText1);
+});
 
 // chart part for lust story
-var ctx2 = document.getElementById('myChart2').getContext('2d');
-
-var barChart = new Chart(ctx2, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
-        datasets: [{
-            label: 'Number',
-            backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
-            borderColor:["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],
-            data: [82, 50, 60, 30, 40, 60, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {
-        legend: {
-            display: false
-        },
-
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-// bubble chart
 var ctx4 = document.getElementById('doughnut-chart').getContext('2d');
 var myDoughnutChart = new Chart(ctx4, {
-    type: 'doughnut',
+    type: 'pie',
     data: {
         labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
         datasets: [{
@@ -511,9 +598,188 @@ var myDoughnutChart = new Chart(ctx4, {
         }]
       },
     options: {
+        animation: false,
         title: {
           display: true,
-          text: 'Lust story'
+          text: 'Lust: Melbourne Pie Chart'
         }
     }
+});
+
+
+$("#table-select a").click(function(e){
+    e.preventDefault(); // cancel the link behaviour
+    var selText = $(this).text();
+    console.log(selText)
+
+    if (selText === "Melbourne"){
+
+        var pieChartContent = document.getElementById('pieChartContent');
+        pieChartContent.innerHTML = '&nbsp;';
+        $('#pieChartContent').append(' <canvas id="doughnut-chart" width="400" height="200"><canvas>');
+        var ctx4 = document.getElementById('doughnut-chart').getContext('2d');
+
+        var myDoughnutChart = new Chart(ctx4, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [82, 50, 60, 30, 40, 30, 45]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Lust: Melbourne Pie Chart'
+                }
+            }
+        });
+    }
+
+    if (selText === "North Melbourne"){
+
+        var pieChartContent = document.getElementById('pieChartContent');
+        pieChartContent.innerHTML = '&nbsp;';
+        $('#pieChartContent').append(' <canvas id="doughnut-chart" width="400" height="200"><canvas>');
+        var ctx4 = document.getElementById('doughnut-chart').getContext('2d');
+        
+        var myDoughnutChart = new Chart(ctx4, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [12, 20, 10, 90, 80, 70, 25]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Lust: North Melbourne Pie Chart'
+                }
+            }
+        });
+    }
+
+    if (selText === "Richmond"){
+
+        var pieChartContent = document.getElementById('pieChartContent');
+        pieChartContent.innerHTML = '&nbsp;';
+        $('#pieChartContent').append(' <canvas id="doughnut-chart" width="400" height="200"><canvas>');
+        var ctx4 = document.getElementById('doughnut-chart').getContext('2d');
+        
+        var myDoughnutChart = new Chart(ctx4, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [36, 29, 80, 20, 40, 30, 95]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Lust: Richmond Pie Chart'
+                }
+            }
+        });
+    }
+    if (selText === "South Melbourne"){
+
+        var pieChartContent = document.getElementById('pieChartContent');
+        pieChartContent.innerHTML = '&nbsp;';
+        $('#pieChartContent').append(' <canvas id="doughnut-chart" width="400" height="200"><canvas>');
+        var ctx4 = document.getElementById('doughnut-chart').getContext('2d');
+        
+        var myDoughnutChart = new Chart(ctx4, {
+            type: 'pie',
+            data: {
+                labels: ['passion', 'desire', 'want', 'joy', 'pleasure', 'longing', 'sensuality'],
+                datasets: [{
+                  label: "Number",
+                  backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#f4a460", "#48d1cc"],
+                  data: [42, 10, 40, 20, 10, 70, 25]
+                }]
+              },
+            options: {
+                animation: false,
+                title: {
+                  display: true,
+                  text: 'Lust: South Melbourne Pie Chart'
+                }
+            }
+        });
+    }
+    $("#table-button").text(selText);
+});
+
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+
+var scatterChartData = {
+    datasets: [{
+        label: 'My First dataset',
+        borderColor: '#3e95cd',
+        backgroundColor: '#3e95cd',
+        data: [{
+            x: 10,
+            y: 20
+        }, {
+            x: 15,
+            y: 10
+        },
+        {
+            x: 11.5,
+            y: 12.3
+        }]
+        
+    }, {
+        label: 'My Second dataset',
+        borderColor:  "#8e5ea2",
+        backgroundColor: "#8e5ea2",
+        data: [{
+            x: 12,
+            y: 15
+        }, {
+            x: 13,
+            y: 12
+        }]
+    }]
+};
+
+
+ var scatterChart2 = new Chart(ctx2, {
+        type: 'scatter',
+        data: scatterChartData,
+        options: {
+            title: {
+                display: true,
+                text: 'Scatter Chart'
+            },
+        }
+    });
+
+$("#wrath-calculate").click(function(e){
+    e.preventDefault(); // cancel the link behaviour
+    var wrath_factor1 = document.getElementById('wrath-factor1').value
+    var wrath_factor2 = document.getElementById('wrath-factor2').value
+    var wrath_outcome = parseInt(wrath_factor1) + parseInt(wrath_factor2)
+    document.getElementById('wrath-outcome').value = wrath_outcome
+});
+
+$("#calculate").click(function(e){
+    e.preventDefault(); // cancel the link behaviour
+    var factor1 = document.getElementById('factor1').value
+    //console.log(factor1)
+    var factor2 = document.getElementById('factor2').value
+    //console.log(factor2)
+    var outcome = parseInt(factor1) + parseInt(factor2)
+    document.getElementById('outcome').value = outcome
 });
